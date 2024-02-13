8d4ee0b295da627313e32ce6651456ac60af906c
ponza555 <ponpon13173@gmail.com>
2024-02-13 19:21:53 +0700

    เสร็จแล่้ว เพิ่ม withdraw ถ้าเวลาเกินแล้ว ละก็ restart game

--- RPS.sol
+++ RPS.sol
@@ -12,8 +12,8 @@ contract RPS is CommitReveal {
     }
     uint public numPlayer = 0;
     uint public reward = 0;
-    uint public timeout = 5 minutes;
-    uint public numberreveal =0 ;
+    uint public timeout = 20 minutes;
+    uint public numberreveal = 0 ;
     uint public numInput = 0;
 
     mapping (uint => Player) public player;
@@ -30,18 +30,22 @@ contract RPS is CommitReveal {
         playernumber[msg.sender] = numPlayer;
         numPlayer++;
     }
-//  input ครบ 2 คนถึงจะยอมเปิด reveal
+//  input ใช้ไม่ไดถ้า reveal แล้วเปลี่ยน การ inputได้เลื่อยๆตาบที่ยังไม่เปิดเผย
     function input(uint choice) public  {
         uint idx = playernumber[msg.sender];
         require(numPlayer == 2);
         require(msg.sender == player[idx].addr);
         require(choice >= 0 && choice < 7);
-        player[idx].timestamps = block.timestamp;
-        player[idx].input = true;
+        require(numberreveal == 0);
+        //เปลี่ยน input ได้
+        if (player[idx].input == false){
+            player[idx].timestamps = block.timestamp;
+            player[idx].input = true;
+            numInput++;
+        }
 // hash ค่าที่เลือก
         bytes32 Hashdata = getHash(bytes32(choice));
         commit(Hashdata);
-        numInput++;
     }
     
     function revealInput(uint choice)public {
@@ -70,5 +74,42 @@ contract RPS is CommitReveal {
         else {
             account0.transfer(reward);
         }
+
+        restartgame();
+    }
+    function withdrawn() public {
+        require(numPlayer == 1 || numPlayer == 2);
+        require(msg.sender == player[0].addr || msg.sender == player[1].addr);
+        uint idx = playernumber[msg.sender];
+        if(numPlayer == 1){
+            idx = 0;
+        }
+        //ไม่ยอม vote 1 คน
+        else if(numPlayer == 2 && numInput < 2){
+            require(player[idx].input == true);
+        }
+        else if (numPlayer == 2 && numInput == 2 && numberreveal < 2 ){
+            require(commits[msg.sender].revealed == true);
+        }
+        require(msg.sender == player[idx].addr);
+        require(block.timestamp - player[idx].timestamps > timeout);
+        address payable  account = payable(player[idx].addr);
+        account.transfer(reward);
+
+        restartgame();
+    }
+    function restartgame() public {
+        numPlayer = 0;
+        reward = 0;
+        numberreveal = 0 ;
+        numInput = 0;
+
+        address account0 = player[0].addr;
+        address account1 = player[1].addr;
+
+        delete playernumber[account0];
+        delete playernumber[account1];
+        delete player[0];
+        delete player[1];
     }
 }
